22ebc6322e0180de70ccb22d52038f4647b723c0
HuaBofeng <148889583+HuaBofeng@users.noreply.github.com>
2024-08-23 13:31:25 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'LANNI', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '这里是LANNIの小窝 科技和日常文章的分享区', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.lanni.us.kg', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.lanni.us.kg', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,lanni,LANNI666,hwafeng,suyutong,huabofeng,华博枫,lanniblog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
